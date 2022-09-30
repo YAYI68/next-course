38c8a5dafaa08731b4668e583c9f22113ff4035f
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-30 16:21:06 -0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,20 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from "next/link";
+import EventList from '../components/events/EventList';
 
+
+const data = [
+  {id:1, name: 'First event'},
+  {id:2, name: 'Second event'},
+  {id:3, name: 'Third event'},
+];
 
 export default function Home() {
   return (
     <div className={styles.container}>
      <h1>Home page</h1>
-     <ul>
-        <li>
-           <Link href="/product" > Product </Link>
-        </li>
-        <li>
-           <Link href="/about" > About </Link>
-        </li>
-     </ul>
+     <EventList items={data}/>
     </div>
   )
 }
