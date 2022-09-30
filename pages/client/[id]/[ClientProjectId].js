166cd87770a466db1f23981d61d05d8014e21ee5
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-30 13:21:55 -0700

    commit

--- pages/client/[id]/[ClientProjectId].js
+++ pages/client/[id]/[ClientProjectId].js
@@ -5,9 +5,15 @@ import { useRouter } from 'next/router';
 function ClientProjectDetail() {
     const router = useRouter();
     const query = router.query;
+  
+    const backHome = ()=>{
+        router.push('/product')
+    }
+
   return (
     <h1>
         Client Project {query.ClientProjectId} from Client {query.id}
+        <button type="button" onClick={backHome}>Product</button>
     </h1>
   )
 }
--- pages/client/[id]/index.js
+++ pages/client/[id]/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { useRouter } from "next/router";
+import Link from 'next/link';
 
 function ClientPage() {
     const router = useRouter();
     const query = router.query
+
+    const clientProject = [
+        {id:1 , name: "Client project 1"},
+        {id:2 , name: "Client project 2"}
+    ]
+
   return (
     <div>
        <h2>Client {query.id}</h2> 
-       
+       <ul>
+          {clientProject.map((project,index)=>(
+            <li>
+                <Link href={{
+                    pathname:"[id]/[ClientProjectId]",
+                    query:{id:query.id,ClientProjectId:project.id}
+                }} >
+                    {project.name}
+                </Link>
+            </li>
+          ))}
+       </ul>
     </div>
   )
 }
